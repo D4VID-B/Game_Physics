ad247969dd82037169f834cbd157cc32378fbbd6
David <davidbakaleynik1998@gmail.com>
2019-10-01 22:44:07 +0200

    Creating CollisionMAnager function

---
+++ GP_Lab/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionManager : MonoBehaviour
+{
+
+    Collision desc;
+
+    Scene currentScene;
+    List<GameObject> colliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliders = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foreach(GameObject obj in )
+        //{
+
+        //}
+    }
+}
