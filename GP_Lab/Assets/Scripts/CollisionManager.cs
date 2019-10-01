eda880200b5003860386a24c8b5c5cbf27c966c0
David <davidbakaleynik1998@gmail.com>
2019-10-01 22:50:07 +0200

    working of collisionManager

--- GP_Lab/Assets/Scripts/CollisionManager.cs
+++ GP_Lab/Assets/Scripts/CollisionManager.cs
@@ -10,19 +10,33 @@ public class CollisionManager : MonoBehaviour
 
     Scene currentScene;
     List<GameObject> colliders;
+    List<GameObject> rootObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         colliders = new List<GameObject>();
+        rootObjects = new List<GameObject>();
+        currentScene.GetRootGameObjects(rootObjects);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //foreach(GameObject obj in )
-        //{
+        foreach (GameObject obj in rootObjects)
+        {
+            if(obj.tag == "Collider")
+            {
+                colliders.Add(obj);
+            }
+        }
 
-        //}
+        checkForCollisions();
     }
+
+    void checkForCollisions()
+    {
+
+    }
+
 }
