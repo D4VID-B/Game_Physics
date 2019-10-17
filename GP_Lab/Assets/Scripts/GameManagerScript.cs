1ca7a9b37c603880023ea248f74c079c22230abf
Colton Soneson <colton.soneson@mymail.champlain.edu>
2019-10-17 17:30:58 -0400

    game manager

---
+++ GP_Lab/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool getToEnd;
+    public bool hitByAstroid;
+
+    public int health;
+    public Text healthBar;
+    
+    void Start()
+    {
+        getToEnd = false;
+        hitByAstroid = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void gameState()
+    {
+        if(getToEnd)
+        {
+
+        }
+
+        if(hitByAstroid)
+        {
+
+        }
+    }
+}
