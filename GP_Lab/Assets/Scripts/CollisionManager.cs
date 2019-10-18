21fff9d672da1d0b8a4a9dff978f730665b29eb7
David <davidbakaleynik1998@gmail.com>
2019-10-18 02:06:56 +0200

    Fixing coroutine for player death; trying to optimise collisionManager

--- GP_Lab/Assets/Scripts/CollisionManager.cs
+++ GP_Lab/Assets/Scripts/CollisionManager.cs
@@ -19,6 +19,8 @@ public class CollisionManager : MonoBehaviour
     public bool enableManager = true;
     public bool shouldChangeColor;
 
+    Camera cm;
+
     [Range(0.0f, 1.0f)]
     public float coefficientOfRestitution = 0.8f;
 
@@ -32,6 +34,9 @@ public class CollisionManager : MonoBehaviour
         rootObjects = new List<GameObject>();
         currentScene.GetRootGameObjects(rootObjects);
 
+        cm = Camera.main;
+
+
         findHulls();
     }
 
@@ -41,8 +46,39 @@ public class CollisionManager : MonoBehaviour
         if(enableManager)
         {
             checkForCollisions();
+            //dynamicAdd();
         }
         
+    }
+
+
+
+
+    //Look at every object in the camera view and add it to the list if it is not already on it
+    void dynamicAdd()
+    {
+        bool onList = false;
+        foreach (GameObject obj in rootObjects)
+        {
+            if(isInView(obj))
+            {
+                CollisionHull2D objHull = obj.GetComponent<CollisionHull2D>();
+
+                foreach (CollisionHull2D hull in colliders)
+                {
+                    if (objHull != null && objHull.gameObject == hull.gameObject)
+                    {
+                        //they are equal
+                        onList = true;
+                    }
+                }
+                if(onList == false)
+                {
+                    colliders.Add(objHull);
+                }
+            }
+            
+        }
     }
 
     void findHulls()
@@ -51,10 +87,27 @@ public class CollisionManager : MonoBehaviour
         foreach (GameObject obj in rootObjects)
         {
             hull = obj.GetComponent<CollisionHull2D>();
-            if (hull != null)
+            
+
+            if (hull != null /*&& isInView(obj)*/)
             {
                 colliders.Add(hull);
             }
+        }
+    }
+
+    bool isInView(GameObject obj)
+    {
+        Vector3 pos = cm.WorldToViewportPoint(obj.transform.position);
+        if (pos.x < 0.0 || pos.x > 1.0)
+        {
+            //Outside the camera
+            return false;
+        }
+        else
+        {
+            //Inside camera view
+            return true;
         }
     }
 
@@ -64,32 +117,42 @@ public class CollisionManager : MonoBehaviour
         {
             for(int j = i+1; j < colliders.Count; j++)
             {
-                if (CollisionHull2D.TestCollision(colliders[i], colliders[j], ref desc))
+                if(colliders[i].gameObject.activeSelf == false || colliders[j].gameObject.activeSelf == false)
                 {
-                    if(shouldChangeColor)
-                    {
-                        CollisionHull2D.changeColor(colliders[i].gameObject, true);
-                        CollisionHull2D.changeColor(colliders[j].gameObject, true);
-                    }
-
-                    CollisionHull2D.resolveInterpenetration(ref desc);
-                    CollisionHull2D.updateCollision(ref desc, coefficientOfRestitution);
-
-                    //Kill the player
-                    GameObject.Find("CollisionManagerHost").GetComponent<GameManagerScript>().hitByAstroid = true;
-                    
-
-                    Debug.Log("Objects collided");
+                    Debug.Log("One of the objects is inactive!");
+                    //Don't update
                 }
                 else
                 {
-                    if(shouldChangeColor)
+                    if (CollisionHull2D.TestCollision(colliders[i], colliders[j], ref desc))
                     {
-                        CollisionHull2D.changeColor(colliders[i].gameObject, false);
-                        CollisionHull2D.changeColor(colliders[j].gameObject, false);
+                        if (shouldChangeColor)
+                        {
+                            CollisionHull2D.changeColor(colliders[i].gameObject, true);
+                            CollisionHull2D.changeColor(colliders[j].gameObject, true);
+                        }
+
+                        CollisionHull2D.resolveInterpenetration(ref desc);
+                        CollisionHull2D.updateCollision(ref desc, coefficientOfRestitution);
+
+                        //Kill the player
+                        GameObject.Find("CollisionManagerHost").GetComponent<GameManagerScript>().hitByAstroid = true;
+
+
+                        Debug.Log("Objects collided");
                     }
-                    
+                    else
+                    {
+                        if (shouldChangeColor)
+                        {
+                            CollisionHull2D.changeColor(colliders[i].gameObject, false);
+                            CollisionHull2D.changeColor(colliders[j].gameObject, false);
+                        }
+
+                    }
                 }
+
+                
             }
         }
     }
