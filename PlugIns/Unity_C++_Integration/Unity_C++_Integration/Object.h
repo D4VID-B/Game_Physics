7b864f942bc0019253331dcec6d673486724aac3
David <davidbakaleynik1998@gmail.com>
2019-11-21 23:28:15 +0100

    Resolving differences; Preparing dll for demo

--- PlugIns/Unity_C++_Integration/Unity_C++_Integration/Object.h
+++ PlugIns/Unity_C++_Integration/Unity_C++_Integration/Object.h
@@ -16,86 +16,133 @@ public:
 
 	float normalize()
 	{
+
 		return 0;
 	}
 
 	float magnitude()
 	{
+
 		return 0;
 	}
+
+	Vector3() { x = 0.0f; y = 0.0f; z = 0.0f; }
+	Vector3(float _x, float _y, float _z) { x = _x; y = _y; z = _z; }
+};
+
+struct Transform
+{
+private:
+
+	Vector3 mPosition;
+	Vector3 mRotation;
+	Vector3 mScale;
+
+public:
+
+	Transform()
+	{
+		mPosition = Vector3();
+		mRotation = Vector3();
+		mScale = Vector3();
+	}
+
+	Vector3 getPosition() { return mPosition; }
+	Vector3 getRotation() { return mRotation; }
+	Vector3 getScale() { return mScale; }
+
+	void setPosition(Vector3 newPos) { Vector3 mPosition = newPos; }
+	void setRotation(Vector3 newRot) { mRotation = newRot; }
+	void setScale(Vector3 newSc) { mScale = newSc; }
 };
 
 class Object
 {
 protected:
 
-	Vector3 mPosition;
-	Vector3 mRotation;
-	Vector3 mScale;
+	Transform mTransform;
+	CircleCollider c_Col;
+	BoxCollider b_Col;
 
-	int mObjectType = 0;
 	int mObjectID;
-
-	bool mActive = false;
+	int color = 0;
 
 public:
 
-	Object(Vector3 pos, Vector3 rot, Vector3 scl, int iD) 
+	Object(const Object &other)
 	{
-		mPosition = pos;
-		mRotation = rot;
-		mScale = scl;
-		mObjectID = iD;
+		mTransform = other.mTransform;
+		mObjectID = other.mObjectID;
+		color = other.color;
 	}
 
-	int getType() { return mObjectType; }
+	Object() { mObjectID = -1; };
+
+	Object(int ID) { mObjectID = ID; }
+
+	Object(Vector3 pos, Vector3 rot, Vector3 scl, int iD, int colType) 
+	{
+		mTransform.setPosition(pos);
+		mTransform.setRotation(rot);
+		mTransform.setScale(scl);
+		mObjectID = iD;
+		color = colType; //Temporary
+	}
+
+	Object(Vector3 pos, Vector3 rot, Vector3 scl, int iD, int colType)
+	{
+		mTransform.setPosition(pos);
+		mTransform.setRotation(rot);
+		mTransform.setScale(scl);
+		mObjectID = iD;
+		color = colType; //Temporary
+	}
+
+	void flipColor()
+	{
+		if (color == 0)
+		{
+			color = 1;
+		}
+		else color = 0;
+	}
+
 	int getID() { return mObjectID; }
-
-	Vector3 getPosition() { return mPosition; }
-
-	Vector3 getRotation() { return mRotation; }
-
-	Vector3 getScale() { return mScale; }
-
-	void setPosition(Vector3 newPos) { mPosition = newPos; }
-
-	void setRotation(Vector3 newRot) { mRotation = newRot; }
-
-	void setScale(Vector3 newSc) { mScale = newSc; }
-
-	bool isInUse() { return mActive; }
 };
 
-class Collider : public Object 
+class Collider 
 {
 	//Needed??
+
+	bool checkCollision(); //Some collision functionality
 };
 
-class CircleCollider : public Object
+class CircleCollider : public Collider
 {
 private:
 	float mRadius;
+
 
 public:
 
 	float getRadius() { return mRadius; }
 	void setRadius(float newRad) { mRadius = newRad; }
-	
+
+	CircleCollider() { }
 	CircleCollider(float rad, int iD) 
 	{ 
 		mRadius = rad;
-		mObjectID = iD;
-		mObjectType = 1; 
 	}
 };
 
-class BoxCollider : public Object
+class BoxCollider : public Collider
 {
 private:
 
 	float mLength = 0;
 	float mHeight = 0;
 	float mWidth = 0;
+	//Could be replaced by a Vector3
 
 public:
 
@@ -107,30 +154,29 @@ public:
 	void setHeight(float newHei) { mHeight = newHei; }
 	void setWidth(float newWdt) { mWidth = newWdt; }
 
-	BoxCollider() { mObjectType = 2; }
+	BoxCollider() {}
 	BoxCollider(float length, float height, float width, int iD) 
 	{
 		mLength = length;
 		mHeight = height;
 		mWidth = width;
-		mObjectID = iD;
-		mObjectType = 2;
 	}
 };
 
 
 class RigidBody : public Object
 {
-	//Collision Tests?
+	//Collision Tests here or in collider?
 
 };
 
 class SoftBody : public Object
 {
-
+	//Cloth and stuff
 };
 
 class Particle : public Object
 {
-
+	//Polymorphic class containint both 2D and 3D dunctionality
+	//Or just 3D stuff - z ignored if used in 2D (bool flag)
 };
