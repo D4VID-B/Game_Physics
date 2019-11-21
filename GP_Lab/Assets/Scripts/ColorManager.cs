c627acc2df124c47b9849beec2b76b595ca5fc13
Colton Soneson <colton.soneson@mymail.champlain.edu>
2019-11-21 18:19:59 -0500

    memes

---
+++ GP_Lab/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColorManager : MonoBehaviour
+{
+
+    Scene currentScene;
+
+    List<GameObject> rootObjects;
+
+    public int delayCap = 25;
+    private int delayIter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentScene = SceneManager.GetActiveScene();
+
+        rootObjects = new List<GameObject>();
+        currentScene.GetRootGameObjects(rootObjects);
+
+        findObj();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (delayIter == delayCap)
+        {
+            updateColors();
+            delayIter = 0;
+        }
+        else
+        {
+            delayIter++;
+        }s
+    }
+
+
+    void updateColors()
+    {
+        
+    }
+
+    void findObj()
+    {
+        int iterID = 0;
+        foreach (GameObject obj in rootObjects)
+        {
+            obj.GetComponent<ColorScript>().ID = iterID;
+
+            iterID++;
+        }
+    }
+}
