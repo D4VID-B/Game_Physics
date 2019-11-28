e7c870a85c04fc53ecd310bb75c65b52f82053f7
David <davidbakaleynik1998@gmail.com>
2019-11-28 16:18:41 +0100

    Wroking on the death scene and some other stuff

---
+++ GP_Lab/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+class SwitchScene : MonoBehaviour
+{
+    public string SceneName;
+
+    public void Switch()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
